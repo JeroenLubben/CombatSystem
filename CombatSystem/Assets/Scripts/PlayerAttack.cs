f540405aaf24b4fa6fac1858702c76d3338b4784
JeroenLubben <jeroenlubben@gmail.com>
2019-03-25 16:31:20 +0100

    Just playing around

---
+++ CombatSystem/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private float timeBetweenAttack;
+    public float startTimeBetweenAttack;
+
+    public LayerMask whatIsEnemy;
+    public Transform attackPos;
+    public float attackRange;
+    public int punchDamage;
+    public int kickDamage;
+
+    //private PlayerClass player;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        //player = GetComponent<PlayerClass>();
+    }
+
+    void Update () {
+		
+        //check if cooldown is finished and attack is selected
+        //WILL NEED TO BE TWEAKED WITH COMBO
+        //punch
+        if (timeBetweenAttack <= 0   &&
+            Input.GetButton("Punch"))
+            // && player.currentEnergy >= 10)
+        {
+            //Attack
+            anim.SetBool("isPunching", true);
+            Collider2D[] enemiesToHit = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy); 
+            for(int i =0; i < enemiesToHit.Length; i++)
+            {
+                //enemiesToHit[i].GetComponent<Enemy>().takeDamage(punchDamage);
+            }
+
+            //player.currentEnergy -= 10;
+            timeBetweenAttack = startTimeBetweenAttack;
+        }//end if
+        else
+        {
+            anim.SetBool("isPunching", false);
+            timeBetweenAttack -= Time.deltaTime;
+        }
+
+        //kick
+        if (timeBetweenAttack <= 0   &&
+             Input.GetButton("Kick"))
+             //&& player.currentEnergy >= 20)
+        {
+            //Attack
+            anim.SetBool("isKicking", true);
+            Collider2D[] enemiesToHit = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+            for (int i = 0; i < enemiesToHit.Length; i++)
+            {
+                //enemiesToHit[i].GetComponent<Enemy>().takeDamage(kickDamage);
+            }
+
+            //player.currentEnergy -= 20;
+            timeBetweenAttack = startTimeBetweenAttack * 2;
+        }//end if
+        else
+        {
+            anim.SetBool("isKicking", false);
+            timeBetweenAttack -= Time.deltaTime;
+        }
+
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
---
+++ CombatSystem/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    private Rigidbody2D rigidbody;
+
+    public float movementSpeed = 40f;
+    public Collider[] AttackHitboxes;
+
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+}
